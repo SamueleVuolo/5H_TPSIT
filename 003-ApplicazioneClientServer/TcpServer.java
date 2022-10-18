7f59745a68d0ecb91ba9b9d4983d56203cf79977
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-10-18 11:59:34 +0200

    Create TcpServer.java

---
+++ 003-ApplicazioneClientServer/TcpServer.java
@@ -0,0 +1,75 @@
+package network;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServer {
+	public static void main(String[] args) throws Exception {
+		
+		int severPort=8765;
+		int voc=0;
+		int cons=0;
+		int carsp=0;
+		String clientMsg = "";
+		
+		try {			 
+			// Creazione del socket sul server e ascolto sulla porta
+			ServerSocket serverSocket = new ServerSocket(severPort);
+			System.out.println("Server: in ascolto sulla porta " + severPort);
+
+			// Attesa della connessione con il client
+			Socket clientSocket = serverSocket.accept();
+			
+			// Create input and output streams to read/write data
+			DataInputStream inStream = new DataInputStream(clientSocket.getInputStream());
+			DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
+
+			// Scambio di dati tra client e server
+			while(!clientMsg.equals("quit")) {
+				//Lettura dato da stream di rete
+				clientMsg = inStream.readUTF();
+				System.out.println("Server: ricevuto messaggio " + clientMsg );
+				
+				//Invio dati su stream di rete
+				outStream.writeUTF("Echo from server : "         + clientMsg);
+				outStream.flush();
+				System.out.println("Server: invio messaggio "    + clientMsg );
+			}
+			
+			voc=0;
+			cons=0;
+			carsp=0;
+			
+			System.out.println("La lunghezza del messaggio: " + clientMsg.length() + " caratteri");
+			for(int i=0; i<clientMsg.length(); i++) 
+			{
+				if(clientMsg.charAt(i)==('a')||clientMsg.charAt(i)==('e')||clientMsg.charAt(i)==('i')||clientMsg.charAt(i)==('o')||clientMsg.charAt(i)==('u'))
+					voc++;
+				else 
+					if(clientMsg.charAt(i)==('!')||clientMsg.charAt(i)==(' ')||clientMsg.charAt(i)==('?')||clientMsg.charAt(i)==(':')||clientMsg.charAt(i)==(';')||clientMsg.charAt(i)==(','))
+						carsp++;
+					
+					else
+						cons++;
+			}
+			
+			System.out.println("Server: vocali " + voc);
+			System.out.println("Server: consonanti " + cons);
+			System.out.println("Server: caratteri speciali " + carsp);
+			
+			
+			
+
+			// Close resources
+			serverSocket.close();
+			clientSocket.close();
+			inStream.close();
+			outStream.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
