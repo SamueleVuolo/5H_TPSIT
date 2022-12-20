01f5c1344b83c25ef8a95ed161fc07291a033ec1
SamueleVuolo <113595004+SamueleVuolo@users.noreply.github.com>
2022-12-20 12:01:42 +0100

    Update ServerWeb.java

--- Es03b/network/ServerWeb.java
+++ Es03b/network/ServerWeb.java
@@ -16,27 +16,32 @@ public class ServerWeb {
 		
 		final int SERVER_PORT=8765;
 		String clientMsg = "";
-		
+		String serverMsg = "";
+		boolean flag;
+		// URL url = new URL("https://www.favicon.cc/");
+		// favicon.ico serverMsg += url;
+
 		try {			 
 			// Creazione del socket sul server e ascolto sulla porta
 			ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
 			System.out.println("Server: in ascolto sulla porta " + SERVER_PORT);
 
-			boolean endConn=false;
-			while(!endConn) {
+			flag=false;
+			while(!flag) {
 				// Attesa della connessione con il client
-				System.out.println("Attesa ricezione dati dal client ....................... \n");
+				System.out.println("\nAttesa ricezione dati dal client ....................... \n");
 				Socket clientSocket = serverSocket.accept();
 				
 				// Create output stream to write data and input stream to read data from socket
 				DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
 				BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	
+
+
 				// ---------------------------------------------------------
 				//Lettura dati dal client un righa alla volta   
-				while ((clientMsg=inStream.readLine()).length() != 0) {
-					System.out.println(clientMsg);	
-				}  
+				clientMsg=inStream.readLine();
+				System.out.println(clientMsg);
+
 				// Elaborare qui i dati ricevuti dal client 
 				// ---------------------------------------------------------
 
@@ -45,7 +50,7 @@ public class ServerWeb {
 				//clientMsg += "Connection: close\r\n";
 				//clientMsg += "Content-Type: text/plain\r\n";
 				clientMsg += "\r\n";
-				clientMsg += "Saluti da Samuele Vuolo";
+				clientMsg += "Saluti da Samuele Vuolo.. " +serverMsg;
 				outStream.write(clientMsg.getBytes());
 				outStream.flush();
 
